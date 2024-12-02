dc0744bfd07030803f6d0918a17e631cb0ed9c8c
izZERO <crusherh7x@hotmail.com>
2024-12-02 13:29:59 +0300

    Enhance authentication flow: add fullname and phoneNumber to user data, integrate DynamoDB for user storage, and update API permissions.

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -44,6 +44,7 @@ export function ApiStack({ stack }: StackContext) {
         function: {
           handler: "packages/functions/src/Authentication/VerifyChallenge.handler",
           runtime: "nodejs18.x",
+          permissions: ["dynamodb:PutItem"],
         },
       },
     },
